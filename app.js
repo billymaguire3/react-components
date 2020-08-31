7b09089748131c5d980560cd93cdcd74229d90ac
William Maguire <billymaguire3@gmail.com>
2020-08-31 14:45:47 -0400

    Implement props to our grocery list

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
-// TODO
+var GroceryList = (props) => {
+
+  var onFirstListItemClicked = (event) => {
+    console.log('Clicked on Item');
+  };
+
+  return (
+    <ul>
+      <li onClick={onFirstListItemClicked}>{props.groceryItems[0]}</li>
+      <li>{props.groceryItems[1]}</li>
+      <li>{props.groceryItems[2]}</li>
+    </ul>
+  );
+}
+
+
+var GroceryListItem = () => (
+  <GroceryList groceryItems={['Apples', 'Peanut Butter', 'Cheese']}/>
+);
+
+ReactDOM.render(<GroceryListItem />, document.getElementById('app'));
